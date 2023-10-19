12344ddac8cc609dfa80d048aa875df99b459d76
Jonny Burger <jonathanburger11@gmail.com>
2023-10-19 17:29:04 +0200

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -6,7 +6,7 @@ import { VERSION } from "remotion/version";
  */
 export const REGION = "us-east-1";
 
-export const SITE_NAME = "my-next-app";
+export const SITE_NAME = "contributor-graph";
 export const RAM = 3009;
 export const DISK = 2048;
 export const TIMEOUT = 240;
